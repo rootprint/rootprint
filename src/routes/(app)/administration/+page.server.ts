4c821984d01f24c8ce5c1cc60db6ec46ba3ba9f0
Oleksandr Zhyhalo <szhyhalo@gmail.com>
2026-03-20 10:55:11 +0100

    Refactor load function to throw error for unauthorized access instead of redirecting

--- src/routes/(app)/administration/+page.server.ts
+++ src/routes/(app)/administration/+page.server.ts
@@ -1,8 +1,8 @@
-import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = (event) => {
 	if (event.locals.user?.role !== 'admin') {
-		throw redirect(302, '/');
+		throw error(403, 'Forbidden');
 	}
 };
