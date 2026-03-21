bcedcc3d0f4e3084b3b246fa8720fdc5285c20ab
Oleksandr Zhyhalo <szhyhalo@gmail.com>
2026-03-21 11:52:15 +0100

    Add $lib alias to vitest config
    This change allows importing modules from `src/lib` directly without
    using relative paths in Vitest tests.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib')
+		}
+	},
 	test: {
 		include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
 		coverage: {
